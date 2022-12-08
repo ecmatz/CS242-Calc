3e1361f74cedd2ce4b9d022943f9264f56d130e4
ecmatz <matzekec@clarkson.edu>
2022-12-08 17:27:51 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cs242calc {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.cs242calc to javafx.fxml;
+    exports com.example.cs242calc;
+}
