680ba5d5b67f9afbc82cb3c69c2f44702d4959fe
Tyler Yankee <tsyankee57@gmail.com>
2022-12-08 17:40:12 -0500

    Add basic definition of Calculator class

---
+++ src/main/java/com/example/cs242calc/Calculator.java
@@ -0,0 +1,17 @@
+package com.example.cs242calc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Calculator extends Application
+{
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
